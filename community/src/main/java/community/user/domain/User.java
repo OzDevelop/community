d07734930ece3c6327ac6f3346527c664b893814
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-04-23 21:16:23 +0900

    Feat: make SpringProject

---
+++ community/src/main/java/community/user/domain/User.java
@@ -0,0 +1,29 @@
+package user.domain;
+
+import java.util.Objects;
+
+public class User {
+    private final Long id;
+    private String username;
+    private String profileImageUrl;
+
+    public User(Long id, String username, String profileImageUrl) {
+        this.id = id;
+        this.username = username;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
