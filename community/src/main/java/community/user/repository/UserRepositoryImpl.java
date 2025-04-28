8ddd63f16bf3bd613302f92900efb57303efdf3e
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-04-28 21:13:44 +0900

    Feat: Add UserService, Dto, Repository

---
+++ community/src/main/java/community/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,16 @@
+package community.user.repository;
+
+import community.user.application.interfaces.UserRepository;
+import community.user.domain.User;
+
+public class UserRepositoryImpl implements UserRepository {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+}
