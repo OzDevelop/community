c35c04b89fddcbea97d58ae23504e12b8f4bda71
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-04-30 15:14:58 +0900

    Feat: Add createUser Method

--- community/src/main/java/community/user/service/UserService.java
+++ community/src/main/java/community/user/service/UserService.java
@@ -1,4 +1,24 @@
 package community.user.service;
 
+import community.user.application.dto.CreateUserRequestDto;
+import community.user.application.interfaces.UserRepository;
+import community.user.domain.User;
+import community.user.domain.UserInfo;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //TODO - Create User
+    public User createUser(CreateUserRequestDto dto) {
+        UserInfo info = new UserInfo(dto.name(), dto.profileImageUrl());
+        User user = new User(null, info);
+
+        return userRepository.save(user);
+    }
+
 }
