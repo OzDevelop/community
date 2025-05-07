adbb7b22df1a5c18e20486faa6ca76acda4eeff0
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-07 15:01:08 +0900

    Feat: Add FakeObjectFactory

---
+++ community/src/test/java/community/Fake/FakeObjectFactory.java
@@ -0,0 +1,33 @@
+package community.Fake;
+
+import community.post.application.CommentService;
+import community.post.application.PostService;
+import community.post.application.interfaces.CommentRepository;
+import community.post.application.interfaces.LikeRepository;
+import community.post.application.interfaces.PostRepository;
+import community.post.repository.FakeCommentRepository;
+import community.post.repository.FakeLikeRepository;
+import community.post.repository.FakePostRepository;
+import community.user.application.interfaces.UserRelationRepository;
+import community.user.application.interfaces.UserRepository;
+import community.user.application.repository.FakeUserRelationRepository;
+import community.user.application.repository.FakeUserRepository;
+import community.user.service.UserRelationService;
+import community.user.service.UserService;
+
+public class FakeObjectFactory {
+
+    private static final UserRepository fakeUserRepository = new FakeUserRepository();
+    private static final UserRelationRepository fakeUserRelationRepository = new FakeUserRelationRepository();
+    private static final PostRepository fakePostRepository = new FakePostRepository();
+    private static final CommentRepository fakeCommentRepository = new FakeCommentRepository();
+    private static final LikeRepository fakeLikeRepository = new FakeLikeRepository();
+
+    private static final UserService userService = new UserService(fakeUserRepository);
+    private static final UserRelationService userRelationRepository = new UserRelationService(
+            fakeUserRelationRepository, userService);
+    private static final PostService postService = new PostService(userService, fakePostRepository, fakeLikeRepository);
+    private static final CommentService commentService = new CommentService(userService, postService, fakeCommentRepository, fakeLikeRepository);
+
+    private FakeObjectFactory() {}
+}
