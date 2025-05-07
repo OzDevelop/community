10f493ff943001c48de3890813796d6a912e828e
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-07 14:53:55 +0900

    Feat: Add FakeRepository(Comment, Post, Like)

---
+++ community/src/test/java/community/post/repository/FakeCommentRepository.java
@@ -0,0 +1,29 @@
+package community.post.repository;
+
+import community.post.application.interfaces.CommentRepository;
+import community.post.domain.comment.Comment;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FakeCommentRepository implements CommentRepository {
+
+    private final Map<Long, Comment> store = new HashMap<>();
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() != null) {
+            store.put(comment.getId(), comment);
+            return comment;
+        }
+
+        long id = store.size() + 1;
+        Comment newComment = new Comment(id, comment.getPost(), comment.getAuthor(), comment.getContentText());
+        store.put(id, newComment);
+        return newComment;
+    }
+
+    @Override
+    public Comment findById(Long id) {
+        return store.get(id);
+    }
+}
---
+++ community/src/test/java/community/post/repository/FakeLikeRepository.java
@@ -0,0 +1,74 @@
+package community.post.repository;
+
+import community.post.application.interfaces.LikeRepository;
+import community.post.domain.Post;
+import community.post.domain.comment.Comment;
+import community.user.domain.User;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FakeLikeRepository implements LikeRepository {
+
+    private final Map<Post, Set<User>> postLikes = new HashMap<>();
+    private final Map<Comment, Set<User>> commentLikes = new HashMap<>();
+
+    @Override
+    public boolean checkLike(User user, Post post) {
+        if(postLikes.get(post) == null) {
+            return false;
+        }
+        return postLikes.get(post).contains(user);
+    }
+
+    @Override
+    public void like(Post post, User user) {
+        Set<User> users = postLikes.get(post);
+        if(users == null) {
+            users = new HashSet<>();
+        }
+        users.add(user);
+        postLikes.put(post, users);
+    }
+
+    @Override
+    public void unlike(Post post, User user) {
+        Set<User> users = postLikes.get(post);
+        if (users == null) {
+            return;
+        }
+        users.remove(user);
+        postLikes.put(post, users);
+
+    }
+
+    @Override
+    public boolean checkLike(Comment comment, User user) {
+        if (commentLikes.get(comment) == null) {
+            return false;
+        }
+        return commentLikes.get(comment).contains(user);
+    }
+
+    @Override
+    public void like(Comment comment, User user) {
+        Set<User> users = commentLikes.get(comment);
+        if (users == null) {
+            users = new HashSet<>();
+        }
+        users.add(user);
+        commentLikes.put(comment, users);
+
+    }
+
+    @Override
+    public void unlike(Comment comment, User user) {
+        Set<User> users = commentLikes.get(comment);
+        if (users == null) {
+            return;
+        }
+        users.remove(user);
+        commentLikes.put(comment, users);
+    }
+}
