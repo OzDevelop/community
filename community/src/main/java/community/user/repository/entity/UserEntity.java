1c00dfe903099edbe86b03cdf338bb7ae4422f45
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-09 20:48:27 +0900

    Feat: Add UserEntity

---
+++ community/src/main/java/community/user/repository/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package community.user.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "community_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String profileImage;
+    private Integer followerCount;
+    private Integer followingCount;
+
+    
+
+
+}
