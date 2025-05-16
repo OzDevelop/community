e472e17602856b871444697bff87f7efeee096a2
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-16 14:58:07 +0900

    Feat: Add UserPostQueueQueryRepoImpl, Dto

---
+++ community/src/main/java/community/post/repository/UserPostQueueQueryRepositoryImpl.java
@@ -0,0 +1,74 @@
+package community.post.repository;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import community.post.application.dto.GetPostContentResponseDto;
+import community.post.application.interfaces.UserPostQueueQueryRepository;
+import community.post.repository.entity.like.QLikeEntity;
+import community.post.repository.entity.post.QPostEntity;
+import community.post.repository.entity.post.QUserPostQueueEntity;
+import community.user.repository.entity.QUserEntity;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserPostQueueQueryRepositoryImpl implements UserPostQueueQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    private final QUserPostQueueEntity userPostQueueEntity = QUserPostQueueEntity.userPostQueueEntity;
+    private final QPostEntity postEntity = QPostEntity.postEntity;
+    private final QUserEntity userEntity = QUserEntity.userEntity;
+    private final QLikeEntity likeEntity = QLikeEntity.likeEntity;
+
+    @Override
+    public List<GetPostContentResponseDto> getContentResponse(Long userId, Long lastContentId) {
+
+        return queryFactory
+                .select(
+                        Projections.fields(
+                                GetPostContentResponseDto.class,
+                                postEntity.id.as("id"),
+                                postEntity.content.as("content"),
+                                userEntity.id.as("userId"),
+                                userEntity.name.as("userName"),
+                                userEntity.profileImage.as("userProfileImage"),
+                                postEntity.commentCount.as("commentCount"),
+                                postEntity.likeCount.as("likeCount"),
+                                likeEntity.isNotNull().as("isLikedByMe")
+                        )
+                )
+                .from(userPostQueueEntity)
+                .join(postEntity).on(userPostQueueEntity.postId.eq(postEntity.id))
+                .join(userEntity).on(userPostQueueEntity.authorId.eq(userEntity.id))
+                .leftJoin(likeEntity).on(hasLike(userId))
+                .where(
+                       userPostQueueEntity.userId.eq(userId),
+                       hasLastData(lastContentId)
+                )
+                .orderBy(userPostQueueEntity.postId.desc())
+                .limit(20)
+                .fetch();
+    }
+
+    private BooleanExpression hasLastData(Long lastId) {
+        if (lastId == null) {
+            return null;
+        }
+        return postEntity.id.lt(lastId);
+    }
+
+    private BooleanExpression hasLike(Long userId) {
+        if (userId == null) {
+            return null;
+        }
+        return postEntity.id
+                .eq(likeEntity.id.targetId)
+                .and(likeEntity.id.targetType.eq("POST"))
+                .and(likeEntity.id.userId.eq(userId));
+    }
+}
