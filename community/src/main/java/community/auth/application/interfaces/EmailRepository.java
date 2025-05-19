0e6f6d7718f1d4850bd1811a1bc7f955b21d5b14
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-19 20:00:34 +0900

    Refactor: Separate Email send, verify Repository

--- community/src/main/java/community/auth/application/interfaces/EmailRepository.java
+++ community/src/main/java/community/auth/application/interfaces/EmailRepository.java
@@ -3,10 +3,7 @@ package community.auth.application.interfaces;
 import community.auth.domain.Email;
 
 public interface EmailRepository {
-    void sendEmail(Email email);
+    void sendEmail(Email email, String subject, String htmlBody);
 
-    // 인증 관련 메서드
-    void createEmailVerification(Email email, String token);
-    void verifyEmail(Email email, String token);
-    boolean isEmailVerified(Email email);
+
 }
