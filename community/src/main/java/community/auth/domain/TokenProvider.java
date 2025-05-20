7016da6d8dd977b3dbb7bbae564b9d12eed664cc
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-20 14:35:30 +0900

    Feat: Add TokenProvider

---
+++ community/src/main/java/community/auth/domain/TokenProvider.java
@@ -0,0 +1,36 @@
+package community.auth.domain;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+    private final SecretKey key;
+    private static final long TOKEN_EXPIRATION_TIME = 1000L * 60 * 60; // 1 hour
+
+    public TokenProvider(@Value("${secret-key}") String key) {
+        this.key = Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+    public String createToken(Long userId, String role) {
+        Claims claims = Jwts.claims()
+                .subject(userId.toString())
+                .build();
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + TOKEN_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(expirationDate)
+                .claim("role", role)
+                .signWith(key)
+                .compact();
+    }
+}
