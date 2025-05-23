2882a880767e1d50a7cb9904f11edf6cc80637aa
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-05-23 14:16:20 +0900

    Feat: Add Feed Acceptance Steps, Test

---
+++ community/src/test/java/community/acceptance/steps/FeedAcceptanceSteps.java
@@ -0,0 +1,39 @@
+package community.acceptance.steps;
+
+import community.post.application.dto.CreatePostRequestDto;
+import community.post.application.dto.GetPostContentResponseDto;
+import io.restassured.RestAssured;
+import java.util.List;
+import org.springframework.http.MediaType;
+
+
+public class FeedAcceptanceSteps {
+
+    public static Long requestCreatePost(CreatePostRequestDto dto) {
+        return RestAssured
+                .given().log().all()
+                .body(dto)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/post")
+                .then().log().all()
+                .extract()
+                .jsonPath()
+                .getObject("value", Long.class);
+    }
+
+    public static List<GetPostContentResponseDto> requestFeedList(String token) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .header("Authorization", "Bearer " + token)
+                .get("/feed")
+                .then().log().all()
+                .extract()
+                .jsonPath()
+                .getList("value", GetPostContentResponseDto.class);
+
+    }
+
+}
