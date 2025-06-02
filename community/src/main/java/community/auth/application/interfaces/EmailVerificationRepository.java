a626d257b9cc017832a52a24c9e4ffc64825d9ea
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-02 14:57:21 +0900

    Refactor: Remove Exception in Repository
    (to Service)

--- community/src/main/java/community/auth/application/interfaces/EmailVerificationRepository.java
+++ community/src/main/java/community/auth/application/interfaces/EmailVerificationRepository.java
@@ -1,10 +1,15 @@
 package community.auth.application.interfaces;
 
 import community.auth.domain.Email;
+import community.auth.repository.entity.EmailVerificationEntity;
+import java.util.Optional;
 
 public interface EmailVerificationRepository {
     // 인증 관련 메서드
     void createEmailVerification(Email email, String token);
     void verifyEmail(Email email, String token);
     boolean isEmailVerified(Email email);
+
+    Optional<EmailVerificationEntity> getEmailVerificationEntity(Email email);
+    void markEmailAsVerified(EmailVerificationEntity entity);
 }
