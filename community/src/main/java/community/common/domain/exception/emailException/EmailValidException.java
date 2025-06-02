ce2092c737e6a1da75630c960669a1cc3a075741
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-02 16:20:32 +0900

    Refactor: change extend class

--- community/src/main/java/community/common/domain/exception/emailException/EmailValidException.java
+++ community/src/main/java/community/common/domain/exception/emailException/EmailValidException.java
@@ -3,7 +3,7 @@ package community.common.domain.exception.emailException;
 import community.common.domain.exception.ErrorCode;
 import community.common.domain.exception.ExceptionBase;
 
-public class EmailValidException extends ExceptionBase {
+public class EmailValidException extends EmailException {
     public EmailValidException()
     {
       super(ErrorCode.INVALID_EMAIL);
