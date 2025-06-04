6b65054a69124726aae08b99deb236c959dcaba4
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-04 13:11:13 +0900

    Refactor: Add Password Exception

--- community/src/main/java/community/auth/domain/password/Password.java
+++ community/src/main/java/community/auth/domain/password/Password.java
@@ -8,10 +8,6 @@ public class Password {
     private final String encryptedPassword;
 
     public static Password createEncryptedPassword(String password) {
-        if(password == null || password.isEmpty()) {
-            throw new IllegalArgumentException("Password cannot be null or empty");
-        }
-
         PasswordValidator.validate(password);
 
         return new Password(SHA256.encrypt(password));
