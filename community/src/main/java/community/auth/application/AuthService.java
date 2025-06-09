d3a484ea7c4112a72159f8eea06769dde477e920
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-09 20:00:46 +0900

    Refactor: change import path

--- community/src/main/java/community/auth/application/AuthService.java
+++ community/src/main/java/community/auth/application/AuthService.java
@@ -6,7 +6,7 @@ import community.auth.application.dto.UserAccessTokenResponseDto;
 import community.auth.application.interfaces.EmailVerificationRepository;
 import community.auth.application.interfaces.UserAuthRepository;
 import community.auth.domain.Email;
-import community.auth.domain.token.TokenProvider;
+import community.auth.domain.TokenProvider;
 import community.auth.domain.UserAuth;
 import community.common.domain.exception.emailException.AlreadyVerifiedEmailException;
 import community.common.domain.exception.emailException.EmailNotVerifiedException;
