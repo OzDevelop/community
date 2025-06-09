5b8e7a723bc904ff4dca0e6b2040b6d5da8b7eac
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-09 14:29:50 +0900

    Feat: Add Tree CommentBuilder

--- community/src/main/java/community/post/application/CommentService.java
+++ community/src/main/java/community/post/application/CommentService.java
@@ -1,5 +1,7 @@
 package community.post.application;
 
+import static community.common.domain.comment.CommentTreeBuilder.commentBuilddTree;
+
 import community.post.application.dto.CreateCommentRequestDto;
 import community.post.application.dto.GetCommentListResponseDto;
 import community.post.application.dto.LikeRequestDto;
@@ -10,7 +12,10 @@ import community.post.domain.Post;
 import community.post.domain.comment.Comment;
 import community.user.domain.User;
 import community.user.application.service.UserService;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -79,7 +84,9 @@ public class CommentService {
     }
 
     public List<GetCommentListResponseDto> getCommnetList(Long postId) {
-        return commentRepository.getCommentList(postId);
+        List<GetCommentListResponseDto> flatList = commentRepository.getCommentList(postId);
+
+        return commentBuilddTree(flatList);
     }
 
     public void deleteComment(Long commentId) {
