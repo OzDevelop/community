aa830630e5b83d3a0a5f79c59808fec24db4243a
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-06-09 20:01:55 +0900

    Feat: Apply SpringSecurity in Feed

--- community/src/main/java/community/post/ui/FeedController.java
+++ community/src/main/java/community/post/ui/FeedController.java
@@ -1,12 +1,16 @@
 package community.post.ui;
 
-import community.auth.domain.token.TokenProvider;
+import community.auth.domain.TokenProvider;
+import community.common.config.CustomUserDetails;
 import community.common.ui.Response;
 import community.post.application.dto.GetPostContentResponseDto;
 import community.post.application.interfaces.UserPostQueueQueryRepository;
+import community.post.repository.entity.post.UserPostQueueEntity;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,19 +25,10 @@ public class FeedController {
 
     @GetMapping
     public Response<List<GetPostContentResponseDto>> getPostFeedList(
-            @RequestHeader("Authorization") String authorization,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam(required = false) Long lastPostId) {
 
-        if(authorization == null || !authorization.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("유효하지 않은 토큰 형식입니다.");
-        }
-
-        String token = authorization.split(" ")[1];
-        Long userId = tokenProvider.getUserId(token);
-
-        System.out.println("userId: " + userId);
-        System.out.println("token: " + token);
-        System.out.println("lastPostId: " + lastPostId);
+        Long userId = userDetails.getUserId();
 
         List<GetPostContentResponseDto> contentResponse = userPostQueueQueryRepository.getContentResponse(userId, lastPostId);
 
