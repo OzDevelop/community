9280345d2ea32e7f1e144159f15775e36d2ae976
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-16 16:13:30 +0900

    refactor: GlobalExceptionHandler, Response 수정

--- community/src/main/java/community/common/ui/GlobalExceptionHandler.java
+++ community/src/main/java/community/common/ui/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import community.common.domain.exception.emailException.EmailException;
 import community.common.domain.exception.passwordException.PasswordException;
 import community.common.domain.exception.postException.PostException;
 import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,41 +14,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ExceptionBase.class)
+    public Response<Void> handleCustomException(ExceptionBase e, HttpServletRequest request) {
+        log.warn("Business error: {}", e.getMessage());
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public Response<Void> handleIllegalArgumentException(IllegalArgumentException e) {
-        log.warn("Invalid input: {}", e.getMessage());
-        return Response.error(ErrorCode.INVALID_INPUT_VALUE);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public Response<Void> handleException(Exception e) {
-        log.warn("Invalid input: {}", e.getMessage());
-        return Response.error(ErrorCode.INTERNAL_ERROR);
-    }
-
-    @ExceptionHandler(MalformedJwtException.class)
-    public Response<Void> handleMalformedJwtException(MalformedJwtException e) {
-        log.warn("Invalid input: {}", e.getMessage());
-        return Response.error(ErrorCode.INVALID_INPUT_VALUE);
-    }
-
-    @ExceptionHandler(EmailException.class)
-    public Response<Void> handleEmailException(EmailException e) {
-        log.warn("Email error: {}", e.getMessage());
-        return Response.error(e.getErrorCode());
-    }
-
-    @ExceptionHandler(PasswordException.class)
-    public Response<Void> handleEmailException(PasswordException e) {
-        log.warn("Password error: {}", e.getMessage());
-        return Response.error(e.getErrorCode());
-    }
-
-
-    @ExceptionHandler(PostException.class)
-    public Response<Void> handleEmailException(PostException e) {
-        log.warn("Post error: {}", e.getMessage());
         return Response.error(e.getErrorCode());
     }
 }
