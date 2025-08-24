25e569469f69c403a2ac5f15c4b7dcfdc33e86a9
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-08-24 18:12:07 +0900

    refactor: GetUserListResponseDto QueryProjection 추가

--- community/src/main/java/community/user/application/dto/GetUserListResponseDto.java
+++ community/src/main/java/community/user/application/dto/GetUserListResponseDto.java
@@ -1,4 +1,16 @@
 package community.user.application.dto;
 
-public record GetUserListResponseDto(String name, String profileImageUrl) {
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+@Getter
+public class GetUserListResponseDto {
+    private final String name;
+    private final String profileImage;
+
+    @QueryProjection
+    public GetUserListResponseDto(String name, String profileImage) {
+        this.name = name;
+        this.profileImage = profileImage;
+    }
 }
